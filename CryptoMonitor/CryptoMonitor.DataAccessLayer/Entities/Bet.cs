38818647c11755a2560ca7a90a5556f0187af5d0
neformatic <reznik.workspace@gmail.com>
2022-03-28 20:42:14 +0300

    AddBetEntity

---
+++ CryptoMonitor/CryptoMonitor.DataAccessLayer/Entities/Bet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace CryptoMonitor.DAL.Entities
+{
+    public class Bet
+    {
+        [Key]
+        public int Id { get; set; }
+        public decimal? BetPrice { get; set; }
+        public int? UserId { get; set; }
+        public int? CurrencyId { get; set; }
+        [ForeignKey("UserId")]
+        public virtual User User { get; set; }
+        [ForeignKey("CurrencyId")]
+        public virtual CryptoCurrency Currency { get; set; }
+    }
+}
---
+++ CryptoMonitor/CryptoMonitor.DataAccessLayer/Migrations/20220328170654_AddBetEntity.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CryptoMonitor.DAL.Migrations
+{
+    public partial class AddBetEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
